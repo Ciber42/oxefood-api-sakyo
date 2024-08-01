92cf41fda3a55c5745a0f10cd46b8dec877aed66
Edilson sakyo <116815521+Ciber42@users.noreply.github.com>
2024-08-01 17:46:47 -0300

    Add files via upload

---
+++ src/main/java/br/com/ifpe/oxefood_api_sakyo/OxefoodApiSakyoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_api_sakyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiSakyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiSakyoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefood_api_sakyo/OxefoodApiSakyoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood_api_sakyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiSakyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
